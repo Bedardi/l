2fbf1a44bd4595ada2b4c11c398c8712fe573afe
Bedardi Bhai <153967679+Bedardi@users.noreply.github.com>
2025-10-22 15:09:58 +0530

    Add multiple applications to apps object

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const apps = {
     "App1": "/apps/calculator/index.html",
-    "calculator": "/apps/calculator/index.html"
+    "calculator": "/apps/calculator/index.html",
+    "App2": "/apps/app2/index.html",
+    "App3": "/apps/app3/index.html",
+    "App4": "/apps/app4/index.html",
+    "App5": "/apps/app5/index.html",
+    "App6": "/apps/app6/index.html",
+    "App7": "/apps/app7/index.html",
+    "App8": "/apps/app8/index.html",
+    "App9": "/apps/app9/index.html",
+    "App10": "/apps/app10/index.html",
+    "App11": "/apps/app11/index.html",
+    "App12": "/apps/app12/index.html",
+    "App13": "/apps/app13/index.html",
+    "App14": "/apps/app14/index.html",
+    "App15": "/apps/app15/index.html",
+    "App16": "/apps/app16/index.html",
+    "App17": "/apps/app17/index.html",
+    "App18": "/apps/app18/index.html",
+    "App19": "/apps/app19/index.html",
+    "App20": "/apps/app20/index.html",
     // नए ऐप्स यहाँ जोड़ें
 };
 
